69c537aacce71e6baed70e356fa6daab3727abb0
jakazzy <jakazzy@gmail.com>
2020-04-17 12:43:32 +0000

    add changes to the space app

--- offline-quickstart-lab/app/service-worker.js
+++ offline-quickstart-lab/app/service-worker.js
@@ -1,1 +1,34 @@
+const cacheName = 'cache-v1'
+const preCacheResources = [
+    '/',
+    '/index.html',
+    'index.html',
+    'styles/main.css',
+    'images/space1.jpg',
+    'images/space2.jpg',
+    'images/space3.jpg'
+]
 
+
+self.addEventListener('install',  event =>{
+    event.defaultPrevented
+    self.skipWaiting()
+  console.log('Service worker install event')
+  event.waitUntil(
+      caches.open(cacheName)
+      .then(cache => {return cache.addAll(preCacheResources)})
+  )
+})
+
+self.addEventListener('activate', event=>{
+    console.log(' Service worker Activate event')
+})
+
+self.addEventListener('fetch', event =>{
+    console.log('Fetch event request: ', event.request.url)
+    event.respondWith( caches.match(event.request)
+    .then(cachedResponse => {
+        if(cachedResponse){ return cachedResponse}
+        else { return fetch(event.request)}
+    }))
+})
